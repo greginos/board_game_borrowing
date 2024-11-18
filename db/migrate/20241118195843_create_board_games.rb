7901af43337e1716724b47d422efed0e102ac6ba
Grégoire Courtois <courtoisgregoire@gmail.com>
2024-11-18 21:03:17 +0100

    init

---
+++ db/migrate/20241118195843_create_board_games.rb
@@ -0,0 +1,9 @@
+class CreateBoardGames < ActiveRecord::Migration[8.0]
+  def change
+    create_table :board_games do |t|
+      t.integer :ean
+      t.string :name
+      t.timestamps
+    end
+  end
+end
