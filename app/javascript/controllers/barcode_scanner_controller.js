fe7fa25d816bc256a59a68f4f86439b085a35985
Grégoire Courtois <courtoisgregoire@gmail.com>
2025-01-13 22:29:41 +0100

    trying to add canvas

--- app/javascript/controllers/barcode_scanner_controller.js
+++ app/javascript/controllers/barcode_scanner_controller.js
@@ -1,3 +1,4 @@
+import Quagga from "https://cdn.jsdelivr.net/npm/@ericblade/quagga2/+esm";
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
@@ -6,12 +7,12 @@ export default class extends Controller {
   connect() {
     console.log("Barcode scanner connected");
 
-    window.Quagga.init(
+    Quagga.init(
       {
         inputStream: {
           name: "Live",
           type: "LiveStream",
-          target: this.videoTarget, // Vidéo cible
+          target: document.querySelector("#video"), // Vidéo cible
           constraints: {
             width: 640,
             height: 480,
@@ -19,9 +20,8 @@ export default class extends Controller {
           },
         },
         decoder: {
-          readers: ["code_128_reader", "ean_reader"], // Types de codes-barres pris en charge
+          readers: ["ean_reader"], // Types de codes-barres pris en charge
         },
-        locate: true,
       },
       (err) => {
         if (err) {
@@ -29,11 +29,43 @@ export default class extends Controller {
           return;
         }
         console.log("Quagga initialized");
-        window.Quagga.start(); // Lancer la caméra
+        Quagga.start(); // Lancer la caméra
       }
     );
 
-    window.Quagga.onDetected((data) => {
+    // Dessin des cadres
+    const canvas = document.querySelector("#canvas");
+    const context = canvas.getContext("2d");
+
+    Quagga.onProcessed((result) => {
+      console.log("Résultat onProcessed :", result);
+      context.clearRect(0, 0, canvas.width, canvas.height); // Efface le canvas
+
+      if (result) {
+        // Si des boîtes (zones détectées) sont trouvées
+        if (result.boxes) {
+          console.log("Zones détectées :", result.boxes);
+          result.boxes
+            .filter((box) => box !== undefined)
+            .forEach((box) => {
+              context.beginPath();
+              context.moveTo(box[0][0], box[0][1]);
+              context.lineTo(box[1][0], box[1][1]);
+              context.lineTo(box[2][0], box[2][1]);
+              context.lineTo(box[3][0], box[3][1]);
+              context.closePath();
+              context.lineWidth = 2;
+              context.strokeStyle = "blue"; // Couleur du cadre
+              context.stroke();
+            });
+        } else {
+          console.log("Aucune zone détectée."); 
+      }
+    }});
+
+
+
+    Quagga.onDetected((data) => {
       const code = data.codeResult.code;
       console.log("Code detected:", code);
       this.resultTarget.value = code; // Mettre à jour le champ
