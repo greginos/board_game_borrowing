9b8423e583792186e3ffb001000582e24971600f
Grégoire Courtois <courtoisgregoire@gmail.com>
2025-03-25 22:24:23 +0100

    add unaccent to the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_23_211433) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_25_212313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "unaccent"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
