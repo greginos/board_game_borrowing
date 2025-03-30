f6af69f12e03ecf0989e939d83be5611724155b3
Grégoire Courtois <courtoisgregoire@gmail.com>
2025-03-30 15:31:18 +0200

    trying something else

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
+import "./controllers"
 import "bootstrap"
 import "jquery"
 import "quagga"
 import "./components/board_game"
 import "@popperjs/core"
-import { Application } from "@hotwired/stimulus";
-import BarcodeScannerController from "./controllers/barcode_scanner_controller";
+import { Application } from "@hotwired/stimulus"
+import BarcodeScannerController from "./controllers/barcode_scanner_controller"
 
-const application = Application.start();
-application.register("barcode-scanner", BarcodeScannerController);
+const application = Application.start()
+application.register("barcode-scanner", BarcodeScannerController)
