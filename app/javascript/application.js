75af7100d732916c4719b7c10641230d865ea942
Grégoire Courtois <courtoisgregoire@gmail.com>
2025-03-30 19:33:57 +0200

    cleanup js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "bootstrap"
 import "jquery"
-import "quagga"
-import "./components/board_game"
-import "@popperjs/core"
+import "bootstrap"
 import { Application } from "@hotwired/stimulus"
-import DropdownController from "./controllers/dropdown_controller"
 
 const application = Application.start()
-application.register("dropdown", DropdownController)
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
+pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "jquery" # @3.7.1
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
-pin "quagga", to: "https://cdn.jsdelivr.net/npm/quagga@0.12.1/dist/quagga.min.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js", preload: true
