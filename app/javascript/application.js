be12129aa96973c466ab5959d8fc127368ebaac0
Grégoire Courtois <courtoisgregoire@gmail.com>
2025-03-30 15:37:48 +0200

    trying something else

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,7 +7,9 @@ import "quagga"
 import "./components/board_game"
 import "@popperjs/core"
 import { Application } from "@hotwired/stimulus"
-import BarcodeScannerController from "./controllers/barcode_scanner_controller"
+// import BarcodeScannerController from "./controllers/barcode_scanner_controller"
+import DropdownController from "./controllers/dropdown_controller"
 
 const application = Application.start()
-application.register("barcode-scanner", BarcodeScannerController)
+// application.register("barcode-scanner", BarcodeScannerController)
+application.register("dropdown", DropdownController)
